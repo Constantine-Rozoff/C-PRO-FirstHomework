b3b2868b8ac40ddc20c7329ebb857e2b4ce7afb2
Constantine-Rozoff <rozov888@gmail.com>
2024-11-03 08:16:56 -0400

    task 2

--- C-PRO-FirstHomework/AlphabetChars.cs
+++ C-PRO-FirstHomework/AlphabetChars.cs
@@ -1,16 +1,17 @@
-namespace MyNamespace;
+namespace C_PRO_FirstHomework;
 
 internal class AlphabetChars
 {
     public static void Main(string[] args)
     {
+        //Task 1
         char[] alphabet =
         {
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
             'x', 'y', 'z'
         };
 
-        char selectedLetter = 'z';
+        char selectedLetter = 'g';
         Console.WriteLine("Selected letter is: " + selectedLetter);
 
         int letterPosition = Array.IndexOf(alphabet, selectedLetter);
@@ -30,5 +31,17 @@ internal class AlphabetChars
         alphabet[letterPosition] = char.Parse(a);
         
         Console.WriteLine("{0}", string.Join(", ", alphabet));
+        
+        
+        //Task 2
+        string cities = "London, Paris, Rome";
+        string symbol = ",";
+
+        string[] splittedCities = StringSplitter.SplitLine(cities, symbol);
+
+        foreach (var city in splittedCities)
+        {
+            Console.WriteLine(city);
+        }
     }
 }
---
+++ C-PRO-FirstHomework/StringSplitter.cs
@@ -0,0 +1,31 @@
+﻿namespace C_PRO_FirstHomework;
+
+public class StringSplitter
+{
+    public static string[]  SplitLine(string line, string symbol)
+    {
+        int position;
+        int count = 0;
+        string[] splittedString = new string[10];
+        do
+        {
+            position = line.IndexOf(symbol, StringComparison.Ordinal);
+
+            if (position >= 0)
+            {
+                string city = line.Substring(0, position);
+
+                splittedString[count] = city;
+                count++;
+            
+                line = line.Substring(position + 2);
+            }
+            else
+            {
+                splittedString[count] = line;
+            }
+        } while (position > 0);
+        
+        return splittedString;
+    }
+}
