d8e14292d9cb211adc5f4efada9f2ee9047e1ee2
Constantine-Rozoff <rozov888@gmail.com>
2024-11-07 13:18:34 -0400

    second homework task 1

---
+++ C-PRO-FirstHomework/ArrayReverse.cs
@@ -0,0 +1,22 @@
+﻿namespace C_PRO_FirstHomework;
+
+public class ArrayReverse
+{
+    public static int[] ReverseArray(int[] arr)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left < right)
+        {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+        
+        return arr;
+    }
+}
--- C-PRO-FirstHomework/MainClass.cs
+++ C-PRO-FirstHomework/MainClass.cs
@@ -1,21 +1,26 @@
-﻿namespace C_PRO_FirstHomework;
+﻿using System.Threading.Channels;
+using static C_PRO_FirstHomework.ArrayReverse;
+
+namespace C_PRO_FirstHomework;
 
 public class MainClass
 {
     public static void Main(string[] args)
     {
+        //Homework 1
         //Task 1
         char[] alphabet =
         {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w',
             'x', 'y', 'z'
         };
 
         char selectedLetter = 'm';
-        
+
         AlphabetChars.GetLetterRowNumberAndChangeCase(alphabet, selectedLetter);
-        
-        
+
+
         //Task 2
         string cities = "London, Paris, Rome";
         string symbol = ",";
@@ -26,13 +31,13 @@ public class MainClass
         {
             Console.WriteLine(city);
         }
-        
+
         //Task 3
         string mainString = "London, Paris, Rome";
         string subString = "Rome";
 
         bool doesMainStringContainSubString = StringContains.ContainsSubstring(mainString, subString);
-        
+
         if (doesMainStringContainSubString)
         {
             Console.WriteLine("Does main string contain substring: yes!");
@@ -41,11 +46,11 @@ public class MainClass
         {
             Console.WriteLine("Does main string contain substring: no!");
         }
-        
+
         //Task 4
         int number = 313;
         NumberReader.ReadNumber(number);
-        
+
         //Task 5
         int a = 5;
         int b = 2;
@@ -55,5 +60,18 @@ public class MainClass
         a = a - b;
 
         Console.WriteLine("a = " + a + ", " + "b = " + b);
+        
+        //Homework 2
+        Console.WriteLine("----------------------------------------");
+
+        //Task 1
+        
+        int[] numbers = [1, 2, 3, 4, 5];
+        int[] reversedArray = ReverseArray(numbers);
+
+        for (int i = 0; i < reversedArray.Length; i++)
+        {
+            Console.WriteLine(reversedArray[i]);
+        }
     }
 }
